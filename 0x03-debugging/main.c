4af47b2d0b3f328058e56530585ae67899d828e5
Ayikpalouis <louisayikpa380@gmail.com>
2023-02-22 10:51:21 -0800

    function similar to the one you worked with in an earlier C project

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int a;
+	int i;
 
-	a = 0;
-	positive_or_negative(a);
+	i = 98;
+	positive_or_negative(i);
 
 	return (0);
 }
